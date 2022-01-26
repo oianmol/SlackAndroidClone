41d5fa786f973e35a1be70312481f675ab23c88b
Anmol Verma <anmol.verma4@gmail.com>
2022-01-26 15:38:15 +0530

    animated visibility for getting started

--- ui-onboarding/src/main/java/dev/baseio/slackclone/uionboarding/compose/GettingStarted.kt
+++ ui-onboarding/src/main/java/dev/baseio/slackclone/uionboarding/compose/GettingStarted.kt
@@ -1,12 +1,14 @@
 package dev.baseio.slackclone.uionboarding
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -18,6 +20,7 @@ import com.google.accompanist.insets.statusBarsPadding
 import dev.baseio.slackclone.commonui.theme.*
 import dev.baseio.slackclone.navigator.ComposeNavigator
 import dev.baseio.slackclone.navigator.Screen
+import kotlinx.coroutines.delay
 
 @Composable
 fun GettingStartedUI(composeNavigator: ComposeNavigator) {
@@ -44,11 +47,7 @@ fun GettingStartedUI(composeNavigator: ComposeNavigator) {
             .fillMaxWidth()
         ) {
           IntroText(modifier = Modifier.padding(top = 12.dp))
-          Image(
-            painter = painterResource(id = R.drawable.gettingstarted),
-            contentDescription = "Logo",
-            Modifier
-          )
+          CenterImage()
           Spacer(Modifier.padding(8.dp))
           GetStartedButton(composeNavigator)
         }
@@ -60,54 +59,119 @@ fun GettingStartedUI(composeNavigator: ComposeNavigator) {
 }
 
 @Composable
-private fun GetStartedButton(composeNavigator: ComposeNavigator) {
-  Button(
-    onClick = {
-      composeNavigator.navigate(Screen.SkipTypingScreen.route)
-    },
-    Modifier
-      .fillMaxWidth()
-      .height(40.dp),
-    colors = ButtonDefaults.buttonColors(backgroundColor = SlackCloneTheme.colors.buttonColor)
+private fun CenterImage() {
+
+  var expanded by remember { mutableStateOf(false) }
+
+  LaunchedEffect(Unit) {
+    expanded = !expanded
+  }
+  AnimatedVisibility(
+    visible = expanded, enter = expandIn(
+      expandFrom = Alignment.Center
+    ) + fadeIn(
+      // Fade in with the initial alpha of 0.3f.
+      initialAlpha = 0.3f
+    ),
+    exit = shrinkOut() + fadeOut()
   ) {
-    Text(
-      text = "Get started",
-      style = SlackCloneTypography.subtitle2.copy(color = SlackCloneTheme.colors.buttonTextColor)
+    Image(
+      painter = painterResource(id = R.drawable.gettingstarted),
+      contentDescription = "Logo",
+      Modifier
     )
+  }
+}
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+private fun GetStartedButton(composeNavigator: ComposeNavigator) {
+  var expanded by remember { mutableStateOf(false) }
+
+  LaunchedEffect(Unit) {
+    expanded = !expanded
+  }
+  val density = LocalDensity.current
+
+  AnimatedVisibility(visible = expanded, enter = slideInVertically {
+    // Slide in from 40 dp from the bottom.
+    with(density) { +180.dp.roundToPx() }
+  } + expandVertically(
+    // Expand from the top.
+    expandFrom = Alignment.Top
+  ) + fadeIn(
+    // Fade in with the initial alpha of 0.3f.
+    initialAlpha = 0.3f
+  ),
+    exit = slideOutVertically() + shrinkVertically() + fadeOut()) {
+    Button(
+      onClick = {
+        composeNavigator.navigate(Screen.SkipTypingScreen.route)
+      },
+      Modifier
+        .fillMaxWidth()
+        .height(40.dp),
+      colors = ButtonDefaults.buttonColors(backgroundColor = SlackCloneTheme.colors.buttonColor)
+    ) {
+      Text(
+        text = "Get started",
+        style = SlackCloneTypography.subtitle2.copy(color = SlackCloneTheme.colors.buttonTextColor)
+      )
+    }
   }
 }
 
 @Composable
 private fun IntroText(modifier: Modifier = Modifier) {
-  Text(
-    text = buildAnnotatedString {
-      withStyle(
-        style = SpanStyle(
-          fontFamily = slackFontFamily,
-          fontWeight = FontWeight.Bold, color = Color.White
-        )
-      ) {
-        append("Picture this: a\n")
-      }
-      withStyle(
-        style = SpanStyle(
-          fontFamily = slackFontFamily,
-          fontWeight = FontWeight.Bold,color = Color.White
-        )
-      ) {
-        append("messaging app,\n")
-      }
-      withStyle(
-        style = SpanStyle(
-          SlackLogoYellow,
-          fontFamily = slackFontFamily, fontWeight = FontWeight.Bold
-        )
-      ) {
-        append("but built for\nwork.")
-      }
-    },
-    textAlign = TextAlign.Left,
-    modifier = modifier.fillMaxWidth(),
-    style = SlackCloneTypography.h4
-  )
+  var expanded by remember { mutableStateOf(false) }
+
+  LaunchedEffect(Unit) {
+    expanded = !expanded
+  }
+  val density = LocalDensity.current
+
+  AnimatedVisibility(visible = expanded, enter = slideInHorizontally {
+    // Slide in from 40 dp from the left.
+    with(density) { -380.dp.roundToPx() }
+  } + expandHorizontally(
+    // Expand from the top.
+    expandFrom = Alignment.Start
+  ) + fadeIn(
+    // Fade in with the initial alpha of 0.3f.
+    initialAlpha = 0.3f
+  ),
+    exit = slideOutHorizontally() + shrinkHorizontally() + fadeOut()) {
+    Text(
+      text = buildAnnotatedString {
+        withStyle(
+          style = SpanStyle(
+            fontFamily = slackFontFamily,
+            fontWeight = FontWeight.Bold, color = Color.White
+          )
+        ) {
+          append("Picture this: a\n")
+        }
+        withStyle(
+          style = SpanStyle(
+            fontFamily = slackFontFamily,
+            fontWeight = FontWeight.Bold, color = Color.White
+          )
+        ) {
+          append("messaging app,\n")
+        }
+        withStyle(
+          style = SpanStyle(
+            SlackLogoYellow,
+            fontFamily = slackFontFamily, fontWeight = FontWeight.Bold
+          )
+        ) {
+          append("but built for\nwork.")
+        }
+      },
+      textAlign = TextAlign.Left,
+      modifier = modifier.fillMaxWidth(),
+      style = SlackCloneTypography.h4
+    )
+  }
+
 }
