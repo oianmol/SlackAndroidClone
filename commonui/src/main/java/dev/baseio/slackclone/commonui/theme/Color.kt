104088f5152fa441da976665a656d33ac14ae5a7
Anmol Verma <anmol.verma4@gmail.com>
2022-01-27 14:14:17 +0530

    ui updates

--- commonui/src/main/java/dev/baseio/slackclone/commonui/theme/Color.kt
+++ commonui/src/main/java/dev/baseio/slackclone/commonui/theme/Color.kt
@@ -2,10 +2,10 @@ package dev.baseio.slackclone.commonui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val SlackCloneColor = Color(0xff411845)
+val SlackCloneColor = Color(0xff411540)
 val AppBarColor = SlackCloneColor
 val TextSecondary = Color(0xff5f6368)
-val DarkBackground = Color(0xff121212)
+val DarkBackground = Color(0xff1a1b1e)
 val FunctionalRed = Color(0xffd00036)
 val FunctionalRedDark = Color(0xffea6d7e)
 val SlackLogoYellow = Color(0xffECB22E)
